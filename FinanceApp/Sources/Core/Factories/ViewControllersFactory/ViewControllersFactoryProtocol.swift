2af91cb50c5587dd40f1608c55c447bfc363f293
arthur <arthur.rios007@gmail.com>
2025-06-18 13:01:35 -0300

    fix: ui kit imports

--- FinanceApp/Sources/Core/Factories/ViewControllersFactory/ViewControllersFactoryProtocol.swift
+++ FinanceApp/Sources/Core/Factories/ViewControllersFactory/ViewControllersFactoryProtocol.swift
@@ -6,10 +6,14 @@
 //
 
 import Foundation
+import UIKit
+
 protocol ViewControllersFactoryProtocol: AnyObject {
-    func makeSplashViewController(flowDelegate: SplashFlowDelegate) -> SplashViewController
-    func makeLoginViewController(flowDelegate: LoginFlowDelegate) -> LoginViewController
-    func makeDashboardViewController(flowDelegate: DashboardFlowDelegate) -> DashboardViewController
-    func makeBudgetsViewController(flowDelegate: BudgetsFlowDelegate, date: Date?) -> BudgetsViewController
-    func makeAddTransactionModalViewController(flowDelegate: AddTransactionModalFlowDelegate) -> AddTransactionModalViewController
+  func makeSplashViewController(flowDelegate: SplashFlowDelegate) -> SplashViewController
+  func makeLoginViewController(flowDelegate: LoginFlowDelegate) -> LoginViewController
+  func makeDashboardViewController(flowDelegate: DashboardFlowDelegate) -> DashboardViewController
+  func makeBudgetsViewController(flowDelegate: BudgetsFlowDelegate, date: Date?)
+    -> BudgetsViewController
+  func makeAddTransactionModalViewController(flowDelegate: AddTransactionModalFlowDelegate)
+    -> AddTransactionModalViewController
 }
